0d3fa2b1bb0c780688b22b8d9ce2f7523c562ac1
pascal_lin <pascal_lin@pascal-lindeMacBook-Pro.local>
2018-07-12 11:14:26 +0800

    update

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 #!/usr/bin/env node
 const program = require('commander');
- 
+const download = require('download-git-repo');
+const chalk = require('chalk');
+const ora = require('ora');
+const spinner = ora('downing...');
+
 program.version('0.1.0', '-v, --version')
     .command('init <name>')
     .action((name) => {
-	  console.log(name);
-	  download('http://xxxxxx:9999:HTML5/H5Template#master', name, {clone: true}, (err) => {
-        console.log(err ? 'Error' : 'Success')
+      spinner.start();
+      download('github:pascallin/deco-brick-cli#template', name, (err) => {
+          if (err) {
+            console.log(chalk.red('Error: ' + err.message));
+            console.log(chalk.red('Download failed. Please retry later.'));
+            spinner.fail();
+            return
+          }
+          console.log(chalk.green('Project create successed.'));
+          spinner.succeed();
       })
     });
 program.parse(process.argv);
