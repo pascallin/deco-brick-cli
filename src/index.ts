521c20069094f703b639b2fbe5215443c77dcacc
lin pascal <dunlintan@hk01.com>
2019-11-25 18:27:44 +0800

    refactor: add env and scripts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import { BrickServer } from "deco-brick";
 import log from "./middleware/log";
 import errorHandler from "./middleware/error-handler";
 
+import * as dotenv from "dotenv";
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
+
 class App extends BrickServer {
   start() {
     this.koa.use(log());
@@ -11,7 +16,7 @@ class App extends BrickServer {
 }
 const app = new App({
   controllerPath: __dirname + "/controller",
-  port: 3001,
+  port: process.env.PORT ? parseInt(process.env.PORT, 0) : 3000,
   viewPath: __dirname + "/../view",
 });
 app.start();
